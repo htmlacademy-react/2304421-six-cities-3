78a340d2da6ab242ed7bfd55209cb009f7e7e75f
Sergey Kozhevnikov <sergeykog01@gmail.com>
2026-02-17 18:14:10 +0300

    Creates Logo component

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function LoginPage(): JSX.Element {
   return (
@@ -8,17 +8,7 @@ function LoginPage(): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link" to="/">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width={81}
-                  height={41}
-                />
-              </Link>
-            </div>
+            <Logo />
           </div>
         </div>
       </header>
