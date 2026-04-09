f453878bc47a329804c1476db7b86961301b143c
Sergey Kozhevnikov <sergeykog01@gmail.com>
2026-04-09 19:18:16 +0300

    Creates current-offer-slice.test.ts module

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,65 @@
 import { City } from '../types/city';
+import { OfferDetails } from '../types/offer-details';
+import { Offer } from '../types/offer';
 
 export function getRandomCity(cities: City[]): City {
   const randomInt = Math.floor(Math.random() * cities.length);
   return cities[randomInt];
 }
 
+export const mockOfferDetails: OfferDetails = {
+  id: '0212',
+  title: 'Some title',
+  type: 'Some type',
+  price: 100,
+  city: {
+    name: 'London',
+    location: {
+      latitude: 2343434,
+      longitude: 34343433,
+      zoom: 6,
+    },
+  },
+  location: {
+    latitude: 33423,
+    longitude: 434388,
+    zoom: 7,
+  },
+  isFavorite: false,
+  isPremium: false,
+  rating: 5,
+  description: 'Some description',
+  bedrooms: 3,
+  goods: ['Some good_1'],
+  host: {
+    name: 'Some name',
+    avatarUrl: 'url/32343',
+    isPro: false,
+  },
+  images: ['url/3343'],
+  maxAdults: 3,
+};
+
+export const mockOffer: Offer = {
+  id: '34324',
+  title: 'Some title',
+  offerType: 'hotel',
+  rating: 5,
+  price: 1000,
+  isPremium: false,
+  isFavorite: false,
+  previewImage: 'url/43234',
+  city: {
+    name: 'Some city name',
+    location: {
+      latitude: 34323,
+      longitude: 3234324,
+      zoom: 6,
+    },
+  },
+  location: {
+    latitude: 432433,
+    longitude: 342434,
+    zoom: 342342,
+  },
+};
