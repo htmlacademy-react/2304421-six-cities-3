b6b26147b08e93233e72a73ef2673a51e21c214c
Sergey Kozhevnikov <sergeykog01@gmail.com>
2026-04-17 22:02:04 +0300

    Adds tests for offer-inside-item, offer-image, not-found-page components, creates mock-component, history-rounter component. Creates browser-history.ts. Edits index.tsx

---
+++ src/components/not-found-page/not-found-page.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import NotFoundPage from './not-found-page';
+import { withHistory } from '../../utils/mock-component';
+
+describe('Component: NotFoundPage', () => {
+  it('should render correctly', () => {
+    const expectedHeaderText = '404';
+    const expectedParagraphText = 'Page not found';
+    const expectedLinkText = 'Go home';
+
+    render(withHistory(<NotFoundPage />));
+
+    expect(screen.getByText(expectedHeaderText)).toBeInTheDocument();
+    expect(screen.getByText(expectedParagraphText)).toBeInTheDocument();
+    expect(screen.getByText(expectedLinkText)).toBeInTheDocument();
+  });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import ErrorMessage from './components/error-message/error-message';
 import { checkAuthAction } from './store/api-actions';
+import HistoryRouter from './components/history-router/history-router';
+import browserHistory from './browser-history';
 
 store.dispatch(checkAuthAction());
 
@@ -15,8 +17,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ErrorMessage />
-      <App />
+      <HistoryRouter history={browserHistory}>
+        <ErrorMessage />
+        <App />
+      </HistoryRouter>
     </Provider>
   </React.StrictMode>
 );
