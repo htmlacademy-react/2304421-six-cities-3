3df70656a3218bd66f8e94125687b836bc71d61d
Sergey Kozhevnikov <sergeykog01@gmail.com>
2026-04-20 15:14:55 +0300

    Fixes test for Footer component

--- src/components/footer/footer.test.tsx
+++ src/components/footer/footer.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Footer from './footer';
+import { withHistory } from '../../utils/mock-component-with-history';
 
 describe('Component: Footer', () => {
   it('should render correctly', () => {
@@ -7,12 +8,13 @@ describe('Component: Footer', () => {
     const imgContainerTestId = 'img-container';
     const mockAltText = '6 cities logo';
 
-    render(<Footer />);
+    const preparedComponent = withHistory(<Footer />);
+
+    render(preparedComponent);
 
     expect(screen.getByTestId(imgContainerTestId)).toBeInTheDocument();
     const image = screen.getByAltText(mockAltText);
     expect(image).toBeInTheDocument();
     expect(image).toHaveAttribute('src', mockImgUrl);
-    expect(screen.getByRole('link')).toHaveAttribute('href', 'main.html');
   });
 });
